40b70373902cc76ed32e6d5ab53494460d9518c3
Hyejin Kwon <hanj8980@gmail.com>
2025-12-24 22:38:38 +0900

    페이지 닫기

--- app/page.tsx
+++ app/page.tsx
@@ -32,162 +32,155 @@ export default function Home() {
   const [letters, setLetters] = useState<PrizeRow[]>([]);
   const videoRef = useRef<HTMLVideoElement>(null);
 
-  useEffect(() => {
-    const isFirstWindow = window.localStorage.getItem("isFirstWindow");
+  // useEffect(() => {
+  //   const fetchPrizes = async () => {
+  //     const { data } = (await supabase
+  //       .from("prize")
+  //       .select("id , rank, name, sale_yn")
+  //       .eq("sale_yn", "Y")) as {
+  //       data: PrizeRow[] | null;
+  //     };
+  //     const refined: PrizeRow[] = (data as PrizeRow[]).map((row) => ({
+  //       id: row.id,
+  //       rank: row.rank ?? "",
+  //       name: row.name ?? "",
+  //       sale_yn: row.sale_yn ?? "Y",
+  //     }));
 
-    if (isFirstWindow === "true") {
-      alert(
-        "이미 뽑기를 진행하셨습니다! \n 만약 뽑기를 뽑지 못하셨다면, nav.jin에게 문의주세요!"
-      );
-      return;
-    }
-    const fetchPrizes = async () => {
-      const { data } = (await supabase
-        .from("prize")
-        .select("id , rank, name, sale_yn")
-        .eq("sale_yn", "Y")) as {
-        data: PrizeRow[] | null;
-      };
-      const refined: PrizeRow[] = (data as PrizeRow[]).map((row) => ({
-        id: row.id,
-        rank: row.rank ?? "",
-        name: row.name ?? "",
-        sale_yn: row.sale_yn ?? "Y",
-      }));
+  //     // Assuming each row has a 'prize' field or 'name' field
+  //     setLetters([
+  //       ...appendMissItems(
+  //         refined ?? [],
+  //         { rank: "폭탄", name: "다음기회에" },
+  //         50
+  //       ),
+  //     ]);
+  //   };
+  //   fetchPrizes();
 
-      // Assuming each row has a 'prize' field or 'name' field
-      setLetters([
-        ...appendMissItems(
-          refined ?? [],
-          { rank: "폭탄", name: "다음기회에" },
-          50
-        ),
-      ]);
-    };
-    fetchPrizes();
+  //   // Start title animation
+  //   setTimeout(() => {
+  //     setTitlePosition({ top: "1rem", transform: "translateX(-50%)" });
+  //   }, 700); // Start animation after 0.5s
 
-    // Start title animation
-    setTimeout(() => {
-      setTitlePosition({ top: "1rem", transform: "translateX(-50%)" });
-    }, 700); // Start animation after 0.5s
+  //   // Simulate loading time
+  //   const timer = setTimeout(() => {
+  //     setIsLoading(false);
+  //     // Fade out the background
+  //     setBackgroundOpacity(0.3);
+  //     // After fade, hide background
+  //   }, 2000); // 2 seconds loading
 
-    // Simulate loading time
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-      // Fade out the background
-      setBackgroundOpacity(0.3);
-      // After fade, hide background
-    }, 2000); // 2 seconds loading
+  //   return () => clearTimeout(timer);
+  // }, []);
 
-    return () => clearTimeout(timer);
-  }, []);
+  // useEffect(() => {
+  //   if (isGumballVisible && videoRef.current) {
+  //     videoRef.current.play();
+  //     setTimeout(() => setVideoOpacity(1), 100); // slight delay for fade in
+  //     const video = videoRef.current;
+  //     const handleLoadedMetadata = () => setVideoDuration(video.duration);
+  //     const handleTimeUpdate = async () => {
+  //       if (video.currentTime > videoDuration - 1 && !showBall) {
+  //         const colors = [
+  //           "blue",
+  //           "red",
+  //           "white",
+  //           "yellow",
+  //           "skyblue",
+  //           "green",
+  //           "pink",
+  //           "orange",
+  //         ];
+  //         setBallColor(colors[Math.floor(Math.random() * colors.length)]);
+  //         console.log(letters);
+  //         const prizeInfo = letters[Math.floor(Math.random() * letters.length)];
+  //         setBallLetter(prizeInfo);
+  //         setShowBall(true);
+  //         const { data } = (await supabase.from("prize-own").insert({
+  //           follower: getCookie("threadId") || "unknown",
+  //           prize_id: prizeInfo.id ?? 0,
+  //         })) as { data: PrizeRow[] | null };
+  //         (await supabase
+  //           .from("prize")
+  //           .update({
+  //             sale_yn: "N",
+  //           })
+  //           .eq("id", prizeInfo.id)) as { data: PrizeRow[] | null };
+  //       }
+  //     };
+  //     video.addEventListener("loadedmetadata", handleLoadedMetadata);
+  //     video.addEventListener("timeupdate", handleTimeUpdate);
+  //     return () => {
+  //       video.removeEventListener("loadedmetadata", handleLoadedMetadata);
+  //       video.removeEventListener("timeupdate", handleTimeUpdate);
+  //     };
+  //   }
+  // }, [isGumballVisible, videoDuration, showBall]);
 
-  useEffect(() => {
-    if (isGumballVisible && videoRef.current) {
-      videoRef.current.play();
-      setTimeout(() => setVideoOpacity(1), 100); // slight delay for fade in
-      const video = videoRef.current;
-      const handleLoadedMetadata = () => setVideoDuration(video.duration);
-      const handleTimeUpdate = async () => {
-        if (video.currentTime > videoDuration - 1 && !showBall) {
-          const colors = [
-            "blue",
-            "red",
-            "white",
-            "yellow",
-            "skyblue",
-            "green",
-            "pink",
-            "orange",
-          ];
-          setBallColor(colors[Math.floor(Math.random() * colors.length)]);
-          console.log(letters);
-          const prizeInfo = letters[Math.floor(Math.random() * letters.length)];
-          setBallLetter(prizeInfo);
-          setShowBall(true);
-          const { data } = (await supabase.from("prize-own").insert({
-            follower: getCookie("threadId") || "unknown",
-            prize_id: prizeInfo.id ?? 0,
-          })) as { data: PrizeRow[] | null };
-          (await supabase
-            .from("prize")
-            .update({
-              sale_yn: "N",
-            })
-            .eq("id", prizeInfo.id)) as { data: PrizeRow[] | null };
-        }
-      };
-      video.addEventListener("loadedmetadata", handleLoadedMetadata);
-      video.addEventListener("timeupdate", handleTimeUpdate);
-      return () => {
-        video.removeEventListener("loadedmetadata", handleLoadedMetadata);
-        video.removeEventListener("timeupdate", handleTimeUpdate);
-      };
-    }
-  }, [isGumballVisible, videoDuration, showBall]);
+  return <h1>현재 서비스 점검 중입니다.</h1>;
+  // return (
+  //   <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+  //     {isLoading && (
+  //       <h1
+  //         className="bg-transparent fixed left-1/2 text-lg sm:text-2xl font-bold text-black dark:text-white z-50 text-center transition-all duration-1000 ease-out"
+  //         style={{ top: titlePosition.top, transform: titlePosition.transform }}
+  //       >
+  //         <span className="block sm:inline">Jin의 뽑기 World에</span>{" "}
+  //         <span className="block sm:inline">오신걸 환영합니다!</span>
+  //       </h1>
+  //     )}
+  //     <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start"></main>
+  //     {!isLoading && (
+  //       <Tutorial handleGumble={() => setIsGumballVisible(true)} />
+  //     )}
 
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      {isLoading && (
-        <h1
-          className="bg-transparent fixed left-1/2 text-lg sm:text-2xl font-bold text-black dark:text-white z-50 text-center transition-all duration-1000 ease-out"
-          style={{ top: titlePosition.top, transform: titlePosition.transform }}
-        >
-          <span className="block sm:inline">Jin의 뽑기 World에</span>{" "}
-          <span className="block sm:inline">오신걸 환영합니다!</span>
-        </h1>
-      )}
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start"></main>
-      {!isLoading && (
-        <Tutorial handleGumble={() => setIsGumballVisible(true)} />
-      )}
+  //     <div
+  //       className="fixed inset-0 z-40 transition-opacity duration-1000"
+  //       style={{ opacity: backgroundOpacity }}
+  //     >
+  //       <Image
+  //         src="/hashira.webp"
+  //         alt="Loading background"
+  //         fill
+  //         className="object-cover"
+  //       />
+  //     </div>
 
-      <div
-        className="fixed inset-0 z-40 transition-opacity duration-1000"
-        style={{ opacity: backgroundOpacity }}
-      >
-        <Image
-          src="/hashira.webp"
-          alt="Loading background"
-          fill
-          className="object-cover"
-        />
-      </div>
-      {/* {isGumballVisible && <GumballMachine />} */}
-      {isGumballVisible && (
-        <div className="fixed inset-0 flex items-center justify-center z-50">
-          <video
-            ref={videoRef}
-            preload="auto"
-            style={{
-              opacity: videoOpacity,
-              width: "300px",
-              height: "451px",
-              objectFit: "cover",
-              transition: "opacity 1s ease-in-out",
-            }}
-          >
-            <source src="/gumble.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-        </div>
-      )}
-      {showBall && (
-        <div className="fixed inset-0 flex items-center justify-center z-50">
-          <Ball
-            showBall={showBall}
-            ballColor={ballColor}
-            ballLetter={
-              <>
-                {ballLetter?.rank}
-                <br />
-                {ballLetter?.name}
-              </>
-            }
-          />
-          <Fireworks />
-        </div>
-      )}
-    </div>
-  );
+  //     {isGumballVisible && (
+  //       <div className="fixed inset-0 flex items-center justify-center z-50">
+  //         <video
+  //           ref={videoRef}
+  //           preload="auto"
+  //           style={{
+  //             opacity: videoOpacity,
+  //             width: "300px",
+  //             height: "451px",
+  //             objectFit: "cover",
+  //             transition: "opacity 1s ease-in-out",
+  //           }}
+  //         >
+  //           <source src="/gumble.mp4" type="video/mp4" />
+  //           Your browser does not support the video tag.
+  //         </video>
+  //       </div>
+  //     )}
+  //     {showBall && (
+  //       <div className="fixed inset-0 flex items-center justify-center z-50">
+  //         <Ball
+  //           showBall={showBall}
+  //           ballColor={ballColor}
+  //           ballLetter={
+  //             <>
+  //               {ballLetter?.rank}
+  //               <br />
+  //               {ballLetter?.name}
+  //             </>
+  //           }
+  //         />
+  //         <Fireworks />
+  //       </div>
+  //     )}
+  //   </div>
+  // );
 }
