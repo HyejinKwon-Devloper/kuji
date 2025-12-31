1b515d0fa25b9cf2b314b4734dd3b01256436aed
Hyejin Kwon <hanj8980@gmail.com>
2025-12-31 21:27:31 +0900

    수정

--- app/page.tsx
+++ app/page.tsx
@@ -316,6 +316,7 @@ export default function Home() {
           threadId={threadId}
           coin={totalCoin}
           onClose={handleClosePrizeDrawModal}
+          onCoinUpdate={async () => await handleCoin()}
         />
       )}
     </div>
